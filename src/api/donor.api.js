6741f01912629bca5d3d012f9d411fcc13004dce
Keenlabi <keenlabinc@gmail.com>
2025-04-22 11:46:51 +0100

    add API functions for user and facility registration, login, email verification, and resend verification

---
+++ src/api/donor.api.js
@@ -0,0 +1,13 @@
+import { ApiFetch } from "./fetch"
+
+export const DonorApi = {
+    async register(formData) {
+        const res = await ApiFetch.post(`/api/users/register/donor`, formData)
+        .then((response)=> response)
+        .catch((error)=> { throw error })
+        console.log(res)
+        
+        if(res.status !== 201) throw new Error("There was an error creating new donor, try again.");
+        return res.data
+    }
+}
