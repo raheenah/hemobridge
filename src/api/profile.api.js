47b3ff74131ae4eda123834586f4762475b84b5c
raheenah <raheenah500@gmail.com>
2025-04-23 19:54:46 +0100

    added console.log to profile.api

--- src/api/profile.api.js
+++ src/api/profile.api.js
@@ -2,6 +2,7 @@ import { ApiFetch } from "./fetch";
 
 export const ProfileApi = {
     async fetchMyProfile() {
+        console.log("Fetching profile data...")
         const data = await ApiFetch.get(`/api/profile`)
         .then((response)=> response.data.data)
         .catch((error)=> { throw error })
