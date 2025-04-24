28bf0ab846f3b6d6383ba594794d1627510d7148
raheenah <raheenah500@gmail.com>
2025-04-24 03:22:09 +0100

    console.log profiledata

--- src/api/profile.api.js
+++ src/api/profile.api.js
@@ -2,10 +2,14 @@ import { ApiFetch } from "./fetch";
 
 export const ProfileApi = {
     async fetchMyProfile() {
+        console.log("Fetching profile data...")
         const data = await ApiFetch.get(`/api/profile`)
         .then((response)=> response.data.data)
         .catch((error)=> { throw error })
     
+
+            console.log("Profile fetched:", data);
+
         return data
     }
 }
