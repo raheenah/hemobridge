fa37bf40e995a9059a54320273883533263169f8
Keenlabi <keenlabinc@gmail.com>
2025-04-29 18:27:29 +0100

    feat: add methods for managing donor and facility schedules in DonationApi

--- src/api/donation.api.js
+++ src/api/donation.api.js
@@ -7,5 +7,34 @@ export const DonationApi = {
         .catch((error)=> { throw error })
 
         return res.data
+    },
+
+    fetchDonorSchedules: async (page = 1) => {
+        const response = await ApiFetch.get(`/api/donation/schedule/donor?page=${page}`)
+        .then((response)=> response.data)
+        .catch((error)=> { throw error })
+        return response.data;
+    },
+
+    fetchFacilitySchedules: async (page = 1) => {
+        const response = await ApiFetch.get(`/api/donation/schedule/facility?page=${page}`)
+        .then((response)=> response.data)
+        .catch((error)=> { throw error })
+
+        return response.data;
+    },
+
+    acceptSchedule: async (scheduleId) => {
+        const response = await ApiFetch.post(`/api/donation/schedule/${scheduleId}/accept`)
+        .then((response)=> response.data)
+        .catch((error)=> { throw error })
+        return response.data;
+    },
+
+    declineSchedule: async (scheduleId) => {
+        const response = await ApiFetch.post(`/api/donation/schedule/${scheduleId}/decline`)
+        .then((response)=> response.data)
+        .catch((error)=> { throw error })
+        return response.data;
     }
 }
