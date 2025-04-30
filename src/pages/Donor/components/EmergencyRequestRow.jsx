318b3977aa511fccaf63f2457f683b612836f888
Keenlabi <keenlabinc@gmail.com>
2025-04-30 23:52:09 +0100

    created row method for the emergency request component

---
+++ src/pages/Donor/components/EmergencyRequestRow.jsx
@@ -0,0 +1,44 @@
+import { DateUtils } from '../../../shared/utils/date.utils';
+import { StringUtils } from '../../../shared/utils/string.utils';
+import { ApiDonationScheduleStatus } from "src/shared/constants/donation-schedule.constant";
+
+export default function EmergencyRequestRow({ request, onView, isExpandable = true }) {
+  return (
+    <li className={`px-2 py-1 flex items-start justify-between ${isExpandable ? 'border-1 border-text-gray' : ''}`}>
+      <div className='flex flex-col gap-1'>
+        {
+          request.status !== ApiDonationScheduleStatus.PENDING
+          &&  <p className='flex items-center gap-2'>
+                <span className='text-text-dark-gray font-bold'>Name: </span>
+                {`${StringUtils.capitalize(request.donorId.firstName)} ${StringUtils.capitalize(request.donorId.lastName)}`}
+              </p>
+        }
+        
+        <p className='flex items-start gap-2'>
+          <span className='text-text-dark-gray font-bold'>Blood Type:</span>
+          {request.bloodType}
+        </p>
+        <p className='flex items-start gap-2'>
+          <span className='text-text-dark-gray font-bold'>Units Requested:</span>
+          {request.unitsRequested}
+        </p>
+        <p className='flex items-start gap-2'>
+          <span className='text-text-dark-gray font-bold'>Date:</span>
+          {DateUtils.formatDate(request.preferredDate)}
+        </p>
+        <p className='flex items-start gap-2'>
+          <span className='text-text-dark-gray font-bold'>Time:</span>
+          {DateUtils.formatTime(request.preferredDate)}
+        </p>
+      </div>
+      {isExpandable && (
+        <button
+          onClick={onView}
+          className='bg-background hover:bg-pink w-fit font-bold text-white py-1 px-2'
+        >
+          Donate now
+        </button>
+      )}
+    </li>
+  );
+}
