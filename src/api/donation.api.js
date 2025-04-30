ccf4fdcd336f3d03ad5e6697e71537431a86e9c2
Keenlabi <keenlabinc@gmail.com>
2025-04-30 23:48:26 +0100

    added the status argument to the fetch schedule methods with a default of undefined

--- src/api/donation.api.js
+++ src/api/donation.api.js
@@ -9,15 +9,15 @@ export const DonationApi = {
         return res
     },
 
-    fetchDonorSchedules: async (page = 1) => {
-        const response = await ApiFetch.get(`/api/donation/schedule/donor?page=${page}`)
+    fetchDonorSchedules: async (page = 1, status=undefined) => {
+        const response = await ApiFetch.get(`/api/donation/schedule/donor?page=${page}&status=${status}`)
         .then((response)=> response.data)
         .catch((error)=> { throw error })
         return response.data;
     },
 
-    fetchFacilitySchedules: async (page = 1) => {
-        const response = await ApiFetch.get(`/api/donation/schedule/facility?page=${page}`)
+    fetchFacilitySchedules: async (page = 1, status=undefined) => {
+        const response = await ApiFetch.get(`/api/donation/schedule/facility?page=${page}&status=${status}`)
         .then((response)=> response.data)
         .catch((error)=> { throw error })
 
